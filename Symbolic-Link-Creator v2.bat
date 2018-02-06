d4b3d9b69e7644d2b943a4a6ab23c6f2c610dfcc
Rymert <rymertm@hotmail.com>
2018-02-06 18:54:29 +0200

    Update Symbolic-Link-Creator v2.bat
    Fixed auto folder name detection.

--- Symbolic-Link-Creator v2.bat
+++ Symbolic-Link-Creator v2.bat
@@ -8,13 +8,17 @@ GOTO START_UP
 :START_UP
 	ECHO Starting symbolic link creator.
 	ECHO %fullOriginalPath%
-	FOR /f "delims==" %%F in ("%fullOriginalPath%") do set foldername=%%~nF
+	PAUSE
+	for /f "delims==" %%F in (%fullOriginalPath%) do set foldername=%%~nF
 	ECHO Moving folder is: "%foldername%"
+	PAUSE
 	ECHO If this script fails for some reason, you can copy they files manualy as explained in the readme and run the following command:
+	PAUSE
 	ECHO MKLINK /J %fullOriginalPath% "%destination%\%foldername%"
 	GOTO PREPARE_DESTINATION
 	
 :PREPARE_DESTINATION
+	PAUSE
 	IF EXIST "%destination%\%foldername%" (
 		ECHO "%destination%\%foldername%" exists.
 		GOTO COPY_FOLDER
@@ -25,12 +29,14 @@ GOTO START_UP
 	)
 
 :COPY_FOLDER
+	PAUSE
 	ECHO Destination folder is: %destination%\%foldername%
 	ECHO Copy command: %fullOriginalPath% "%destination%\%foldername%" /E /I /H /K
 	XCOPY %fullOriginalPath% "%destination%\%foldername%" /E /I /H /K
 	GOTO RENAME_OF_ORIGINAL
 
 :RENAME_OF_ORIGINAL
+	PAUSE
 	ECHO Original input is: %fullOriginalPath%
 	ECHO Original name is: %foldername%
 	ECHO %fullOriginalPath% will be renamed to "%foldername% (Original)"
@@ -39,6 +45,7 @@ GOTO START_UP
 	GOTO SYMBOLIC_LINK_CREATION
 
 :SYMBOLIC_LINK_CREATION
+	PAUSE
 	ECHO Creating symbolic link %foldername% from %fullOriginalPath% now...
 	ECHO MKLINK /J %fullOriginalPath% %destination%\%foldername%
 	MKLINK /J %fullOriginalPath% "%destination%\%foldername%"
